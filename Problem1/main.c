025545fe008ee851a19c0a0c837b17c1f5a35299
diana <diana@gmail.com>
2022-02-28 18:33:09 +0200

    initial commit

---
+++ Problem1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a function that takes as an input an array of integer numbers(both positive and negative numbers)  and returns the value of the triplet with the maximum product, as well as the elements that form this triplet.
+// Use pass by pointer / address to return the elements that form that triplet.
+// In the main function, you will first read the size of the array n, and then n integers representing the elements in the array.
+
+int main() {
+	return 0;
+}
---
+++ Problem2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a program that takes a single command line argument: 
+// the path to a file.This file is actually a dictionary that contains, on each line, a word from the given language.
+// You can consider that the maximum size of a word is 50 characters.
+// Display the frequency table of the letters from this file, and then the word with the maximum number of letters, the word with the maximum number of vowels and the word with the maximum number of consonants
+
+int main() {
+	return 0;
+}
